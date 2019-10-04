8ca71cbfc7536c9670fc8099c43f2e850e3c662b
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-10-04 16:21:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -25,24 +25,61 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
+  
   new_array = []
   counter = 0
+  
   while counter < source_array.length do
     new_array << source_array[counter]*2
     counter += 1
   end
+  
   return new_array
 end
 
 def map_to_square(source_array)
+  
   new_array = []
-  counter = 0
+  counter = 
+  
   while counter < source_array.length do
     new_array << source_array[counter]**2
     counter += 1
   end
+  
   return new_array
 end
+
+def reduce_to_total(source_array, starting_point)
+  
+  total = 0
+  counter = 0
+  
+  while counter < source_array.length do
+    total += source_array[counter]
+    counter += 1
+  end
+  return total
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
