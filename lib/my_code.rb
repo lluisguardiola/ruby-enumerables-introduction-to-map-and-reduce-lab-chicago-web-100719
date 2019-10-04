b24a8c000f78671d3be015e55f2884a6d8e0a890
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-10-04 16:28:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -62,6 +62,16 @@ def reduce_to_total(source_array, starting_point=0)
   return starting_point
 end
 
+def reduce_to_all_true(source_array)
+  
+  new_array = []
+  counter = 0
+  while counter < source_array.length do
+    new_array << source_array[counter]*-1
+    counter += 1
+  end
+  return new_array
+end
 
 
 
