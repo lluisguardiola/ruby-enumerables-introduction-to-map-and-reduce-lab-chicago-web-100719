c25f4ddb984f8864ace598c7e86993043c6067c5
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-10-04 16:46:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -83,7 +83,7 @@ def reduce_to_any_true(source_array)
   
   counter = 0
   
-  while counter < source_array.length do
+  while source_array.length > counter do
     return true if source_array[counter] == true
     counter += 1
   end
