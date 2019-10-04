068efee58e2bb19e5e989be10db4a7837d5cb11d
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-10-04 16:23:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,15 +51,14 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-  
-  total = 0
+
   counter = 0
   
   while counter < source_array.length do
-    total += source_array[counter]
+    starting_point += source_array[counter]
     counter += 1
   end
-  return total
+  return starting_point
 end
 
 
