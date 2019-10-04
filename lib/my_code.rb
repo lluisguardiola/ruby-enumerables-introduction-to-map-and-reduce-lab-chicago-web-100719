a84e3b89c68819abd4cac696419228a7aaa27912
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-10-04 16:24:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -56,7 +56,7 @@ def reduce_to_total(source_array, starting_point)
   
   while counter < source_array.length do
     starting_point += source_array[counter]
-    counte1r += 
+    counter += 1
   end
   
   return starting_point
