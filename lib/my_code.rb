e6e894cb745b4912996d8006b3967f3599e2398f
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-10-04 16:19:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -23,3 +23,37 @@ def map_to_no_change(source_array)
   
   return new_array
 end
+
+def map_to_double(source_array)
+  new_array = []
+  counter = 0
+  while counter < source_array.length do
+    new_array << source_array[counter]*2
+    counter += 1
+  end
+  return new_array
+end
+
+def map_to_square(source_array)
+  new_array = []
+  counter = 0
+  while counter < source_array.length do
+    new_array << source_array[counter]**2
+    counter += 1
+  end
+  return new_array
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
