9ad41e2f49539d930a9b8ffaf38ecf9738175b12
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-10-04 16:15:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,8 @@
 def map_to_negativize(source_array)
   counter = 0
   while source_array.length > counter do
-    new_array << source_array[index]
+    new_array << source_array[counter]*-1
+  end
 end
 
 def map_to_no_change(source_array)
